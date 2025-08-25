20d67ff11e4620c7a9979b371f840c244fef7923
emma <emmadomeisen@gmail.com>
2025-08-25 14:25:15 +0200

    Updated Test script

--- backend/debugquit.py
+++
@@ -1,3 +0,0 @@
-import requests
-
-requests.post("http://0.0.0.0:3000/debugquit")
--- backend/testscript.py
+++ backend/testscript.py
@@ -1,3 +1,8 @@
 import requests
 headers = {'Content-type': 'application/json'}
 print(requests.post("http://0.0.0.0:3000/createpage",json={"name":"test","text":'[# phead]__Hello__ _is a word._ [br]`print("hello world");`[#/phead][! infobox][title [:] xyz][img [:] xyz][!/ infobox]'},headers=headers).text)
+print(requests.get("http://0.0.0.0:3000/wiki/test"))
+try:
+    requests.post("http://0.0.0.0:3000/debugquit")
+except:
+    quit()
